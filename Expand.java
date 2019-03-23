bf4ae42fb57ee30fa72e3fbcff73bd0bc4489789
Shravani1510 <shravanikumari321@gmail.com>
2019-03-23 00:46:47 -0700

    please check this task

--- Expand.java
+++ Expand.java
@@ -3,17 +3,43 @@ import java.util.Scanner;
 //advanced
 
 public class Expand {
+	//todo: take as an input a combination of characters and numbers (e.g. 2a3b5c)
+			//expand the String by printing each letter so many times as the number before the letter indicates
+			//e.g. 2a3b5c -> aabbbccccc
+			//Hint: first start with inputs where there is always a number and then a character, then think about how
+			//to improve your code such that you can use values greater or equal 10, eg. 10a13b22c
+			//Further modification: if a character should only be printed one time you don't need to write any number infront of that character
+			//e.g. a3b12cd5e -> abbbccccccccccccdeeeee
 	
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+		String a = "15a3bc";
+		System.out.println(isExpand(a));
 		
-		//todo: take as an input a combination of characters and numbers (e.g. 2a3b5c)
-		//expand the String by printing each letter so many times as the number before the letter indicates
-		//e.g. 2a3b5c -> aabbbccccc
-		//Hint: first start with inputs where there is always a number and then a character, then think about how
-		//to improve your code such that you can use values greater or equal 10, eg. 10a13b22c
-		//Further modification: if a character should only be printed one time you don't need to write any number infront of that character
-		//e.g. a3b12cd5e -> abbbccccccccccccdeeeee  
+	}
+	public static String isExpand(String a) {
+		String c ="";
+		String temp ="0";
+		for(int i=0; i<a.length();i++) {
+			char cheesecake =a.charAt(i);
+			if(Character.isDigit(cheesecake)) {
+				temp += cheesecake;				
+			}
+			else {
+				int n = Integer.parseInt(temp);
+				if(n==0) {
+					c += a.charAt(i);
+				}else {
+				for (int j=0; j<n; j++) {
+					c+=a.charAt(i);
+				}
+			
+				temp="0";
+			}
+				
+		}
+			
+	}
+		return c;
 		
 	}
 	
--- Palindrome.java
+++ Palindrome.java
@@ -4,5 +4,34 @@ public class Palindrome{
 	//A palindrome is a word which can be read backwards and forwards in the same way., e.g. 'hannah'.
 	//Ignore upper and lower case. 'Hannah' is a palindrome, too.
 	//Ignore also spaces. 'Taco cat' is a valid palindrome.
-
+	public static boolean isPalindrome(String c) {
+		String a=c;
+		String b="";
+		
+		for(int i= a.length()-1; i> -1; i--) {
+			if(a.charAt(i)!=' ') {
+				//a.charAt(i);
+				b+=a.charAt(i);
+			}
+		}
+//		System.out.println(b);
+//		if(a.equalsIgnoreCase(b)) {
+//			return true;
+//		}
+//		System.out.println(a.replace(" ", ""));
+		if(a.replace(" ", "").equalsIgnoreCase(b)) {
+			return true;
+		}
+		return false;
+		
+	}
+	public static void main(String[]args) {
+		String c = "Taco cat";
+		System.out.println(isPalindrome(c));
+		
+	}
+	
+	
+	
+	
 }
